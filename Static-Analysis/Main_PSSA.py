d6d9a7d821054eef780c3d81eeb0cc249f530e39
DavidSzO <134441369+DavidSzO@users.noreply.github.com>
2024-08-27 17:46:42 -0300

    modifications 27/08/24

--- Static-Analysis/Main_PSSA.py
+++ Static-Analysis/Main_PSSA.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                             'OneCase': 1,       # (1) for All cases or (2) for Just One Case analysis
                             'SavePath':"C:/Users/David/OneDrive/Documents/FERV_documentos/",
         # ---------------------------------------------------
-                            'generatescript' : False,  # Put TRUE just for generate the script for simulation and saving the flows in Organon
+                            'generatescript' : False,   # Put TRUE just for generate the script for simulation and saving the flows in Organon
                             'OPFsimulation': False,
                             'OnlyPWF_datagen': False,   # Put TRUE just for generate the data for Interconnection and Line Flow Analysis
                             'extract_fromcsv' :False,   # Put TRUE just in the first simulation, once the ProcessedDataBase.csv is generated it is not necessary
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         # ---------------------------------------------------
                             'linhascsv':False,          # Put TRUE once is generated the LinhasInfo file
                             'reservacsv':False,         # Put TRUE once is generated the ReserveInfo file
-                            'HVDCcsv': False,            # Put TRUE once is generated the HVDCinfo file
+                            'HVDCcsv': False,           # Put TRUE once is generated the HVDCinfo file
         # ---------------------------------------------------
                             'PlotGeralPotencia': True,
                             'MapasPlots': True,
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     # path_folder_8 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_2/V2A2F5_REV02_2026/'
 
     # path_folder  = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F2_rev5/'
-    path_folder_11 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F3_rev5/'
+    # path_folder_11 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F3_rev5/'
     # path_folder_2  = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F4_rev5/'
     # path_folder_3 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F5_rev5/'
     #     
@@ -95,8 +95,8 @@ if __name__ == '__main__':
     # path_folder_33  = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V1A1F_/Unicamp/V1A1F4_RESP_in_FNS_lim_Unicamp/'
     # path_folder_44  = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V1A1F_/Unicamp/V1A1F5_RESP_in_FNS_lim_Unicamp/'
 
-    # path_folder_11 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F2_RESP_in_FNS_lim_Unicamp/'
-    # path_folder_22 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F3_RESP_in_FNS_lim_Unicamp/'
+    path_folder_11 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F2_RESP_in_FNS_lim_Unicamp/'
+    path_folder_22 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F3_RESP_in_FNS_lim_Unicamp/'
     # path_folder_33 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F4_RESP_in_FNS_lim_Unicamp/'
     # path_folder_44 = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/Unicamp/V2A2F5_RESP_in_FNS_lim_Unicamp/'
 
@@ -108,7 +108,7 @@ if __name__ == '__main__':
     # ============================= List of PATHS ===========================================
     # path_folders = [path_folder_1, path_folder_2, path_folder_3, path_folder_4, path_folder_5, path_folder_6, path_folder_7, path_folder_8, path_folder_9, path_folder_10, 
     # path_folders = [path_folder_22, path_folder_33, path_folder_44]
-    path_folders = [path_folder_11]
+    path_folders = [path_folder_11, path_folder_22]
     for path_folder in path_folders:
 
         if Options_ReadProcess['generatescript']:
@@ -117,10 +117,10 @@ if __name__ == '__main__':
         elif Options_ReadProcess['OPFsimulation']:
             # =============== PARA GERAR O SCRIPT PARA RODAR O OPF E PWF DOS CASOS NO ORGANON ==================
             # ==================================================================================================
-            path_folderFrom = 'D:/0 FERV/0 Dados PYTHON/CASOS 2026/V2A2F_/REV_5/V2A2F2_rev5/'  #Ese path deve conter as ultimas modificações dos arquivos necesarios para rodar o OPF no Organon (Ex: file.prm, file.def, file.opf, ...)
+            path_folderFrom = 'Static-Analysis/RECURSOS/OPF_files/'  #Ese path deve conter as ultimas modificações dos arquivos necesarios para rodar o OPF no Organon (Ex: file.prm, file.def, file.opf, ...)
             nomefolder = ''
-            OPF = Opf_Simulation(path_folderFrom, nomefolder, path_folder, copypwf=False) # Para gerar as pastas
-            # OPF.OPF_Process()
+            OPF = Opf_Simulation(path_folderFrom, nomefolder, path_folder, copypwf=True) # Para gerar as pastas
+            OPF.OPF_Process()
             OPF.HVDCchanger()
         else:
             cenarios = AnalyzeStaticCases(path=path_folder, Options = Options_ReadProcess)
--- Static-Analysis/Opf_Simulation.py
+++ Static-Analysis/Opf_Simulation.py
@@ -15,48 +15,42 @@ class Opf_Simulation():
         
     def OPF_Process(self):
 
-        path_folder = self.path_folder
+        path_folder = self.path_main
         files_and_directories = os.listdir(path_folder)
         days = [nomes_archivos for nomes_archivos in files_and_directories if 'DS20' in nomes_archivos] 
         days.sort() 
-
-        folder_path = os.path.join(self.path_main)
-        os.makedirs(folder_path, exist_ok=True)
-
         contenidos = []
+
         for dayfolder in days:
             
-            path_to = os.path.join(folder_path, dayfolder)
-            os.makedirs(path_to, exist_ok=True)
-            path_to_output = path_to
-            path_to = path_to + '/Output'
-            os.makedirs(path_to, exist_ok=True)
+            path_ = os.path.join(path_folder, dayfolder)
+            path_to_output = path_ + '/Output'
+            os.makedirs(path_to_output, exist_ok=True)
 
-            path_from = path_folder + dayfolder + '/Output'
-            arquivos_dia = os.listdir(path_from)
-            nomespwf = [i for i in os.listdir(path_to_output)  if (i.startswith('PTOPER_')) & (i.endswith('.pwf')) ]
+            path_from = self.path_folder
+            nomespwf = [i for i in os.listdir(path_)  if (i.startswith('PTOPER_')) & (i.endswith('.pwf')) ]
 
             # Copia cada arquivo ao directorio de destino
             if self.copypwf:
                 for archivo in nomespwf:
                     try: 
-                        ruta_archivo_origen = os.path.join(path_to_output, archivo)
-                        ruta_archivo_destino = os.path.join(path_to, archivo)
+                        ruta_archivo_origen = os.path.join(path_, archivo)
+                        ruta_archivo_destino = os.path.join(path_to_output, archivo)
                         shutil.copy(ruta_archivo_origen, ruta_archivo_destino)
                     except:
                         print('No se encontrou o ' + archivo + ' no directorio')
 
-            for archivo in ['ORGANON.prm','DCShunt_Revisado.def', 'BNT1.dat', 'TENSAO_FPO.opf', 'Model.dyn', 'SEP_BMONTE_ITAIPU_CORB.sps', 'SelectedEvents.evt']: #'TENSAO_FPO.opf' 'Novo.scd',
+            for archivo in ['ORGANON.PRM','DCShunt_Revisado.def', 'BNT1.dat', 'TENSAO_FPO.opf', 'Model.dyn', 'SEP_BMONTE_ITAIPU_CORB.sps', 'SelectedEvents.evt']: #'TENSAO_FPO.opf' 'Novo.scd',
                 try: 
                     ruta_archivo_origen = os.path.join(path_from, archivo)
-                    ruta_archivo_destino = os.path.join(path_to, archivo)
+                    ruta_archivo_destino = os.path.join(path_to_output, archivo)
                     shutil.copy(ruta_archivo_origen, ruta_archivo_destino)
-                    # ruta_archivo_destino_2 = os.path.join(path_to_2, archivo)
+                    # ruta_archivo_destino_2 = os.path.join(path_to_output_2, archivo)
                     # shutil.copy(ruta_archivo_origen, ruta_archivo_destino_2)
                 except:
                     print('No se encontrou o ' + archivo + ' no directorio')
 
-            # *************************************************************************************
+            # ************************************************************************************* 
             # *************************************************************************************
             semi_hours = []
             for hour in range(24):
@@ -70,28 +64,28 @@ class Opf_Simulation():
                 idx2 = idx1 + 1
                 text.append([semi_hours[idx1],semi_hours[idx2],i])
 
-            SCNpath = path_to + "/Novo.scd" 
+            SCNpath = path_to_output + "/Novo.scd" 
             with open(SCNpath, 'w') as f:
                 for idx2, filespwf in enumerate(text):
                     f.write('REF ' + filespwf[0] +', '+ filespwf[1] + ', "' + filespwf[2] +'"')
                     f.write('\n')
-                    if idx2 == len(text)-1:
-                        f.write('END')
+                    # if idx2 == len(text)-1:
+                    #     f.write('END')
 
-            path_script_org = path_to + "/Script_scenarios.txt"  
+            path_script_org = path_to_output + "/Script_scenarios.txt"  
             with open(path_script_org, 'w') as f:
                 
-                f.write('OPEN "' + path_to + '/ORGANON.prm"')
+                f.write('OPEN "' + path_to_output + '/ORGANON.PRM"')
                 f.write('\n')
-                f.write('OPEN "' + path_to + '/Novo.scd"')
+                f.write('OPEN "' + path_to_output + '/Novo.scd"')
                 f.write('\n')
-                f.write('OPEN "' + path_to + '/TENSAO_FPO.opf"')
+                f.write('OPEN "' + path_to_output + '/TENSAO_FPO.opf"')
                 f.write('\n')
-                f.write('OPEN "' + path_to + '/DCShunt_Revisado.def"')
+                f.write('OPEN "' + path_to_output + '/DCShunt_Revisado.def"')
                 f.write('\n')
                 f.write('SCNRUN')
                 f.write('\n')
-                # f.write('ALLNTWPF "' + path_to + '"')
+                # f.write('ALLNTWPF "' + path_to_output + '"')
                 # f.write('\n')
                 
             with open(path_script_org, 'r') as f:
